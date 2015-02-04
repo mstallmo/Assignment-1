4a1f7b8a730d50f014882d98f6c7745f5543b64d
Mason <masonstallmo@yahoo.com>
2015-02-04 13:01:15 -0700

    New Format

--- serverStatic.js
+++ serverStatic.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 app.use(errorHandler({
   dumpExceptions: true,
   showStack: true
